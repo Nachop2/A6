7fa598c21101e9a104a604b50cb4bf76f7fa6cea
Nacho <italiyankina@gmail.com>
2023-09-29 19:33:41 +0100

    Add home view and it's associated route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/inicio', function () {
+    return view('home');
+});
+
 Route::get('/user/{name?}', function (?string $name = "Please input an username") {
     return $name;
 });
